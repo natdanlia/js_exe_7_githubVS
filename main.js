8f877b19565f882d9f0f0cfdb784253ef5839e96
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-07-17 16:59:07 -0400

    html and js are halfway completed

---
+++ main.js
@@ -0,0 +1,43 @@
+let  myName = document.querySelector('.header');
+let ul = document.querySelector('ul');
+
+
+
+
+
+
+
+function reqListener () {
+  let data = JSON.parse(this.responseText);
+  let textcontent = document.createTextNode( data.name);
+  myName.appendChild(textcontent);
+
+
+  let li1 = document.createElement( "li");
+  let li2 = document.createElement( "li");
+  let li3 = document.createElement( "li");
+  let li4 = document.createElement( "li");
+
+  let li1Content = document.createTextNode( `Name: ${data.name}` );
+  let li2Content = document.createTextNode( `Github URL ${data.html_url}`);
+  let li3Content = document.createTextNode( `Company: ${data.company}` );
+  let li4Content = document.createTextNode( `Website: ${data.blog}` );
+
+  ul.appendChild( li1 );
+  ul.appendChild( li2 );
+  ul.appendChild( li3 );
+  ul.appendChild( li4 );
+
+  li1.appendChild( li1Content );
+  li2.appendChild( li2Content );
+  li3.appendChild( li3Content );
+  li4.appendChild( li4Content );
+}
+
+
+
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/natdanlia");
+
+req.addEventListener("load", reqListener);
+req.send();
